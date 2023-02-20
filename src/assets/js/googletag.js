4728acffce7256306faf20201c40509c3bab4faf
gaurav-all-1 <gauravsinghal5678gs678@gmail.com>
2023-02-20 14:01:43 +0530

    first commit

---
+++ src/assets/js/googletag.js
@@ -0,0 +1,17 @@
+function sendConversion(){
+    gtag('event', 'conversion', {
+        'send_to': 'AW-11002071027/r6iYCJ_evoIYEPOPmf4o',
+        'value': 1.0,
+        'currency': 'INR',
+        'transaction_id': ''
+    });
+}
+
+function sendCartConversion()
+{
+    gtag('event', 'conversion',  {
+        'send_to': 'AW-11002071027/gglFCKXxvYIYEPOPmf4o',
+        'value': 1.0,
+        'currency': 'INR'
+    });
+}
